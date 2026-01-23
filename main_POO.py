02147840abde258bf83b254c722796d65b4b3aca
rverla <robin.verla94@gmail.com>
2026-01-23 23:38:42 +0100

    Mise a jour des déplacements Les déplacement sont maintenant anticipé et autorisé ou non en fonction des collisions futures. On part du principe que cette partie est fonctionnelle et finie pour l'instant.

--- main_POO.py
+++ main_POO.py
@@ -13,12 +13,9 @@ class Game:
         self.player = Player(250,250)   # Création du joueur aux coordonnées (250, 250)
         self.zone = pygame.Rect(600,350,300,300)
         self.zone_color = (255,0,0)
-        self.rect_testx = self.player.rect.copy()
+        self.rect_testx = self.player.rect.copy()   # rect test pour les collisions
         self.rect_testy = self.player.rect.copy()
-        self.acces_mx = False
-        self.acces_my = False
-        self.acces_px = False
-        self.acces_py = False
+        self.acces = [False, False, False, False]  # acces mx, my, px, py
 
 
     ### Gestion des evenements ###
@@ -33,17 +30,17 @@ class Game:
         keys = pygame.key.get_pressed()
         
         if keys[K_q]:
-            self.player.direction[0] = -self.player.speed
+            self.player.direction[0] = -1
         elif keys[K_d]:
-            self.player.direction[0] = self.player.speed
+            self.player.direction[0] = 1
         else:
             self.player.direction[0] = 0
 
 
         if keys[K_z]:
-            self.player.direction[1] = -self.player.speed
+            self.player.direction[1] = -1
         elif keys[K_s]:
-            self.player.direction[1] = self.player.speed
+            self.player.direction[1] = 1
         else:
             self.player.direction[1] = 0
     
@@ -52,39 +49,36 @@ class Game:
     def update(self):
         self.player.velocite = [0,0]  # Réinitialisation de la vélocité à chaque frame
         
-        self.acces_mx = False
-        self.acces_my = False
-        self.acces_px = False
-        self.acces_py = False
-        self.rect_testx = self.player.rect.copy()
+        self.acces = [False, False, False, False]  # réinitialisation des accès mx, my, px, py
+        self.rect_testx = self.player.rect.copy()  # réinitialisation des rects tests
         self.rect_testy = self.player.rect.copy()
         
         ## on teste les rect futurs et on autorise ou non le déplacement ##
-        self.rect_testx.x = self.rect_testx.x + self.player.direction[0]
+        self.rect_testx.x = self.rect_testx.x + (self.player.direction[0] * self.player.speed)  # malynx le lynx
         if self.rect_testx.colliderect(self.zone):
-            self.player.velocite[0] = 0
+            self.acces[0],self.acces[2] = False, False
         elif self.player.direction[0] < 0:
-            self.acces_mx = True
+            self.acces[0] = True
         elif self.player.direction[0] > 0:
-            self.acces_px = True
-        
-        
-        self.rect_testy.y = self.rect_testy.y + self.player.direction[1]
+            self.acces[2] = True
+
+
+        self.rect_testy.y = self.rect_testy.y + (self.player.direction[1] * self.player.speed)
         if self.rect_testy.colliderect(self.zone):
-            self.player.velocite[1] = 0
+            self.acces[1],self.acces[3] = False, False
         elif self.player.direction[1] < 0:
-            self.acces_my = True
+            self.acces[1] = True
         elif self.player.direction[1] > 0:
-            self.acces_py = True
-        
-        if self.acces_mx:
-            self.player.velocite[0] = -1
-        if self.acces_px:
-            self.player.velocite[0] = 1
-        if self.acces_my:
-            self.player.velocite[1] = -1
-        if self.acces_py:
-            self.player.velocite[1] = 1
+            self.acces[3] = True
+
+        if self.acces[0]:   #on distribue la vélocité en fonction des accès
+            self.player.velocite[0] = -1    #acces mx
+        if self.acces[2]:
+            self.player.velocite[0] = 1   #acces px
+        if self.acces[1]:
+            self.player.velocite[1] = -1    #acces my
+        if self.acces[3]:
+            self.player.velocite[1] = 1  #acces py
         
         self.player.move()  # on applique le déplacement du joueur
         
